de6f5e5f0640200add9796f7e0ae23cf84c288f8
amansehgalg <amansehgal870@gmail.com>
2019-08-29 18:25:37 +0530

    Added new API KEY

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     router = express.Router(),
-    nodemailer = require('nodemailer');
-    var sgMail = require('@sendgrid/mail');
+    nodeMailer = require('nodemailer');
+    // sgMail = require('@sendgrid/mail');
 
 
 // var User = require('../models/user.js');
@@ -41,31 +41,75 @@ function sendEmail(email, url) {
 
     // create reusable transporter object using the default SMTP transport
 
+    // var transporter = nodemailer.createTransport('smtps://princesehgal452%40gmail.com:PSEHGAL45@smtp.gmail.com');
+    // // setup e-mail data with unicode symbols
+    // var mailOptions = {
+    //     from: '"Prince" <princesehgal452@gmail.com>', // sender address
+    //     to: 'parshotam.kumar32@gmail.com', // list of receivers
+    //     subject: 'Hello ✔', // Subject line
+    //     text: 'Hello world ?', // plaintext body
+    //     html: '<b>Hello world ?</b>' // html body
+    // };
+    //
+    // // send mail with defined transport object
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if(error){
+    //         return console.log(error);
+    //     }
+    //     console.log('Message sent: ' + info.response);
+    // });
 
-    var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'princesehgal452@gmail.com',
-    pass: 'PSEHGAL45'
-  }
-});
 
-    // send mail with defined transport object
-    let info =  transporter.sendMail({
-        from: 'princesehgal452@gmail.com', // sender address
-        to: email, // list of receivers
-        subject: 'Set card details', // Subject line
-        // text: 'Hello world?', // plain text body
-        html: '<strong>Please <a href="'+url+'"> click here</a>  to enter all details </strong>',
 
-    });
+    let transporter = nodeMailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+              // should be replaced with real sender's account
+              user: 'amansehgal870@gmail.com',
+              pass: 'aman03765'
+          }
+      });
+      let mailOptions = {
+          // should be replaced with real recipient's account
+          to: 'parshotam.kumar32@gmail.com',
+          subject: "Set card detail",
+          body: 'tesyee'
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+      });
 
-    console.log('Message sent: %s', info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+//     var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'princesehgal452@gmail.com',
+//     pass: 'PSEHGAL45'
+//   }
+// });
+//     let data = '<strong>Please <a href="[url]"> click here</a>  to enter all details </strong>'
+//     data = data.replace('[url]', url)
+//     // send mail with defined transport object
+//     let info =  transporter.sendMail({
+//         from: 'princesehgal452@gmail.com', // sender address
+//         to: email, // list of receivers
+//         subject: 'Set card details', // Subject line
+//         text: 'Hello world?', // plain text body
+//         html: 'tesyee ',
+//
+//     });
+//     console.log('Message sent: %s', info.messageId);
+//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//
+//     // Preview only available when sending through an Ethereal account
+//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 
@@ -79,6 +123,7 @@ router.post('/savedata', function(req, res) {
         var price = req.body.price
         var currency = req.body.currency
         var url= req.body.url
+        // sendEmail(email,url)
 
         pool.query('INSERT INTO user_data (source, destination, email, price, currency) VALUES ($1, $2, $3, $4, $5)', [source, destination, email, price, currency], (error, results) => {
             if (error) {
@@ -89,10 +134,31 @@ router.post('/savedata', function(req, res) {
             }
 
 
-            sendEmail(email,url);
+
             res.json({
                    'status':'success',
                    'msg': 'Data saved successfully.'
+                })
+          });
+
+});
+
+router.get('/getdata', function(req, res) {
+
+        pool.query('SELECT * from user_data', function(error, results){
+            if (error) {
+              res.json({
+                   'status':'error',
+                   'msg': 'Some error occur.'
+                });
+            }
+
+
+
+            res.json({
+                   'status':'success',
+                   'msg': 'Get Data saved successfully.',
+                    'data':results.rows
                 })
           });
 
